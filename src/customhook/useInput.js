c4bd2fcd08ad98366f372da05e2ef3b94ccd6f8e
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-05-13 08:04:38 +0100

    start

---
+++ src/customhook/useInput.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const useInput = (initialValue = "")=>{
+    const [value,setValue] =  useState(initialValue)
+    const reset = () => {
+        setValue(initialValue)
+    }
+}
